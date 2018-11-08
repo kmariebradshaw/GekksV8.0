18c50b0c31f8bfea32294ec7d86ef867839c8820
Kaitlyn Bradshaw <kaitlynbradshaw@Blairs-MacBook-Pro-2.local>
2018-11-08 15:01:50 -0600

    switches to a css transition for a smooth scroll in and adds a dark overlay to the non cart elements

--- assets/custom.jquery.js
+++ assets/custom.jquery.js
@@ -57,8 +57,10 @@ $('.klaviyo_header_close').on('click', function() {
 });
 $('body').click(function (event) {
    if(!$(event.target).closest('.modal').length && !$(event.target).is('.modal') && !$(event.target).is('#referafriend h3') && !
-    $(event.target).is('#size-chart')) {
+    $(event.target).is('#size-chart')  && !$(event.target).closest('#CartContainer').length) {
      $(".modal").hide(); 
+     $("#CartContainer").hide();
+     console.log(event.target)
    }    
 });
 
