27a36a58055e91784d4b428570f1965f06340989
Kaitlyn Bradshaw <kaitlynbradshaw@Blairs-MacBook-Pro-2.local>
2018-12-04 12:05:38 -0600

    adds an option for order contains a gift that will feed through the cart notes into ship station

--- assets/customcheckout.js
+++ assets/customcheckout.js
@@ -5,5 +5,17 @@ $(document).ready(function() {
     $('.postpurchase').show().insertAfter('.thank-you__additional-content'); 
     createCookie('postpurchase', 'true'); 
   }
+  if ($(".section--shipping-method").length) {
+    $('.gift-option').insertBefore(".section--shipping-method").show(); 
+  }
 }); 
+$(document).ready(function() {
+  $('#gift').change(function() {
+    if ($(this).prop("checked") == true) {
+      jQuery.post('/cart/update.js', {note: "gift"});
+    }
+  })
+})
 
+
+ 
